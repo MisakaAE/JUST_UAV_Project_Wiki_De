c41c2341c58704beed1278633d9737aa8a885b3b
MisakaAE <misakaae@outlook.com>
2024-11-08 20:47:51 +0800

    bash update

--- Start.bat
+++ Start.bat
@@ -5,7 +5,7 @@ title MENU
 
 :menu
 cls
-ECHO. [1]push origin [2]clean [3]server
+ECHO. [1]push origin [2]clean [3]server [0]VSCode
 ECHO.
 echo.select£º
 set ID="q"
@@ -13,10 +13,15 @@ set /p ID="> "
 if "%id%"=="1" goto cmd1
 if "%id%"=="2" goto cmd2
 if "%id%"=="3" goto cmd3
+if "%id%"=="0" goto cmd0
 if "%id%"=="q" goto end
 
 :end 
 exit
+
+:cmd0
+code ./
+goto menu
 
 :cmd1
 git push -u origin main
